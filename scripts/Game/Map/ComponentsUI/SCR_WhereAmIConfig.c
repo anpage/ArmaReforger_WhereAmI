a5dccc7c8c4a4b523f64e7e6bcd5021287cc9a89
Alex Page <alex@anpage.me>
2025-02-12 22:39:28 -0500

    Make direction indicator configurable

---
+++ scripts/Game/Map/ComponentsUI/SCR_WhereAmIConfig.c
@@ -0,0 +1,6 @@
+[BaseContainerProps(configRoot: true)]
+class SCR_WhereAmIConfig
+{
+	[Attribute(defvalue: "1", desc: "Whether or not to show the player's current direction")]
+	bool m_bShowDirection;
+}
--- scripts/Game/Map/ComponentsUI/SCR_WhereAmIUI.c
+++ scripts/Game/Map/ComponentsUI/SCR_WhereAmIUI.c
@@ -3,6 +3,7 @@ class SCR_WhereAmIUI : SCR_MapUIBaseComponent
 	protected Widget m_widget;
 	protected SCR_MapToolEntry m_ToolMenuEntry;
 	protected ImageWidget m_DirectionImage;
+	protected bool m_showDirection = true;
 
 	override void Update(float timeSlice)
 	{
@@ -45,6 +46,11 @@ class SCR_WhereAmIUI : SCR_MapUIBaseComponent
 
 		m_widget = GetGame().GetWorkspace().CreateWidgets("{3513CA0C0C83D2E3}UI/layouts/GPS/PlayerMapIndicator.layout", m_RootWidget);
 		m_DirectionImage = ImageWidget.Cast(m_widget.FindAnyWidget("LineImage"));
+		
+		if (m_showDirection)
+		{
+			m_DirectionImage.SetVisible(true);
+		}
 	}
 
 	//------------------------------------------------------------------------------------------------
@@ -55,6 +61,18 @@ class SCR_WhereAmIUI : SCR_MapUIBaseComponent
 		{
 			m_ToolMenuEntry = toolMenu.RegisterToolMenuEntry("{14A14368AE7FA04D}UI/Textures/Icons/PlayerMarker.imageset", "PlayerMarker", 99);
 			m_ToolMenuEntry.m_OnClick.Insert(ZoomInOnPlayer);
+		}
+		
+		Resource holder = BaseContainerTools.LoadContainer("{44EFCF9D49290924}Configs/Map/WhereAmI.conf");
+		if (!holder)
+		{
+			return;
+		}
+		
+		SCR_WhereAmIConfig config = SCR_WhereAmIConfig.Cast(BaseContainerTools.CreateInstanceFromContainer(holder.GetResource().ToBaseContainer()));
+		if (config)
+		{
+			m_showDirection = config.m_bShowDirection;
 		}
 	}
 
